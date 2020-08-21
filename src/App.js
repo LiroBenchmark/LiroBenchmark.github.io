c4fca1871e4fab6b734cbc00838066e6f0873be5
RePierre <petru.rebeja@gmail.com>
2020-08-21 14:20:12 +0300

    Recreated the app using React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
+import ghLogo from './mark-github.svg';
+import graph from './graph.svg';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+	<>
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="/">
+      <img
+        alt=""
+        src={logo}
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />{' '}
+      RoLa benchmark
+    </Navbar.Brand>
+    <Navbar.Toggle />
+    <Navbar.Collapse className="justify-content-end">
+      <Nav.Link href="/leaderboard">
+      <img src={graph} />
+      {' '}
+      Leaderboard
+    </Nav.Link>
+    <Nav.Link href="https://github.com/eemlcommunity/ro_benchmark_leaderboard" target="_blank">
+      <img src={ghLogo} />
+      {' '}
+      Code
+    </Nav.Link>
+    </Navbar.Collapse>
+  </Navbar>
+  <Container></Container>
+</>
   );
 }
 
