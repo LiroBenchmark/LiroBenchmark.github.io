12ec3887ddffd117fbb6935195cf5a62f70f8c32
RePierre <petru.rebeja@gmail.com>
2020-08-26 15:25:42 +0300

    Added route and component for task details

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Homepage from './Homepage';
+import TaskDetails from './TaskDetails';
 
 const Dashboard = (props) => {
   const {location} = props;
   return (
     <Switch location={location}>
+      <Route exact path="/task/:id" component={TaskDetails} />
       <Route path="/" component={Homepage} />
     </Switch>
-  )
-}
+  );
+};
 
 export default Dashboard;
