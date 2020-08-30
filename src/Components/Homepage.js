3312f352c1183deeb1a70c0af4e73caa6e5848de
sergiu-ojoc <sergiu.ojoc@bytex.ro>
2020-08-30 08:45:02 +0300

    Adding eslint formatting + couple code adjustements

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import DomainTile from './DomainTile';
-import data from '../data/domains.json';
 
-
-const Homepage = () => data.domains.map(item=> <DomainTile key={item.id} id={item.id} name={item.domain} tasks={item.tasks} />)
-// const Homepage = () => <DomainTile />
+const Homepage = () => <DomainTile />;
 
 export default Homepage;
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,42 +1,41 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import {GraphIcon} from '../assets/icons';
+import { GraphIcon } from '../assets/icons';
 import '../assets/menu.scss';
 
-const menuOptions= [
+const menuOptions = [
   {
     // @TODO - need to investigate, current url is /http://...
-    text: "Code",
+    text: 'Code',
     url: 'https://github.com/eemlcommunity/ro_benchmark_leaderboard',
-    target: "_blank",
-    icon: <GraphIcon />
-  },{
-    text: "Datasets",
+    target: '_blank',
+    icon: <GraphIcon />,
+  },
+  {
+    text: 'Datasets',
     url: '/datasets',
-    target: "",
-  },{
-    text: "About",
+    target: '',
+  },
+  {
+    text: 'About',
     url: '/about',
-    target: "",
-  }];
+    target: '',
+  },
+];
 
-const Menu = () => {
-  return(
-    <div className="header-menu">
-      <ul>
-        {menuOptions.map(currentItem => {
-          return (
-          <li key={currentItem.url}>
-            <NavLink exact to={currentItem.url} target={currentItem.target}>
-                {currentItem.icon}
-                <span>{currentItem.text}</span>
-            </NavLink>
-          </li>
-          )
-        })}
-      </ul>
-    </div>
-  )
-}
+const Menu = () => (
+  <div className="header-menu">
+    <ul>
+      {menuOptions.map((currentItem) => (
+        <li key={currentItem.url}>
+          <NavLink exact to={currentItem.url} target={currentItem.target}>
+            {currentItem.icon}
+            <span>{currentItem.text}</span>
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 export default Menu;
