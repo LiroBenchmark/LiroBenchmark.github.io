f311c7e9e93acbefce177e7df5f75e91cd139172
RePierre <petru.rebeja@gmail.com>
2020-09-01 10:45:43 +0300

    Fixed task url to avoid 404 response from GH pages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/ro_benchmark_leaderboard">
     <Route path="/" component={App} />
   </BrowserRouter>,
   document.getElementById('root'),
