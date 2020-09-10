10cb1ccdd3acb5ebc41eadbeeee2211b5067f16a
RePierre <petru.rebeja@gmail.com>
2020-09-10 14:01:54 +0300

    Refactored rendering of task details
    Added new method `renderTaskDatasets()` to display a message when
    there are no submissions for the current task or render the data when
    submissions are present.

--- src/Components/TaskDetails.js
+++ src/Components/TaskDetails.js
@@ -10,56 +10,63 @@ class TaskDetails extends React.Component {
     this.task = data.tasks.find((t) => t.id === this.taskId);
   }
 
+  renderTaskDatasets(datasets) {
+    if (!datasets || datasets.length == 0) {
+      return <p>There are no submissions for this task.</p>;
+    }
+
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <td>Dataset</td>
+            <td>Best model</td>
+            <td>Paper title</td>
+            <td>Paper</td>
+            <td>Code</td>
+            <td>&nbsp;</td>
+          </tr>
+        </thead>
+        <tbody>
+          {datasets.map((ds) => {
+            const { dataset, model_name, paper_title, paper_link, source_link } = ds;
+            return (
+              <tr key={model_name}>
+                <td>{dataset}</td>
+                <td>{model_name}</td>
+                <td>{paper_title}</td>
+                <td>
+                  {paper_link && (
+                    <a href={paper_link} target="_blank" rel="noopener noreferrer">
+                      <PaperIcon />
+                    </a>
+                  )}
+                </td>
+                <td>
+                  {source_link && (
+                    <a href={source_link} target="_blank" rel="noopener noreferrer">
+                      <CodeIcon />
+                    </a>
+                  )}
+                </td>
+                <td>
+                  <a href="#">See all</a>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+
   render() {
     const { datasets } = this.task;
     return (
       <div className="task-details">
         <h3>{this.task.task_name}</h3>
         <p className="task-description">{this.task.task_description}</p>
-
-        {datasets && (
-          <table className="table">
-            <thead>
-              <tr>
-                <td>Dataset</td>
-                <td>Best model</td>
-                <td>Paper title</td>
-                <td>Paper</td>
-                <td>Code</td>
-                <td>&nbsp;</td>
-              </tr>
-            </thead>
-            <tbody>
-              {datasets.map((ds) => {
-                const { dataset, model_name, paper_title, paper_link, source_link } = ds;
-                return (
-                  <tr key={model_name}>
-                    <td>{dataset}</td>
-                    <td>{model_name}</td>
-                    <td>{paper_title}</td>
-                    <td>
-                      {paper_link && (
-                        <a href={paper_link} target="_blank" rel="noopener noreferrer">
-                          <PaperIcon />
-                        </a>
-                      )}
-                    </td>
-                    <td>
-                      {source_link && (
-                        <a href={source_link} target="_blank" rel="noopener noreferrer">
-                          <CodeIcon />
-                        </a>
-                      )}
-                    </td>
-                    <td>
-                      <a href="#">See all</a>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        )}
+        {this.renderTaskDatasets(datasets)}
       </div>
     );
   }
