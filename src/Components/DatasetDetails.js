c2efa7a318928f3de2cdcc9e86e8c315c205a1ab
RePierre <petru.rebeja@gmail.com>
2020-09-11 08:58:42 +0300

    Display dataset title, description, source, and licence

--- src/Components/DatasetDetails.js
+++ src/Components/DatasetDetails.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import data from '../data/datasets.json';
+import { CodeIcon } from '../assets/icons';
+import { PaperIcon } from '../assets/icons';
+import UrlBuilder from './UrlBuilder';
 
 class DatasetDetails extends React.Component {
   constructor(props) {
     super(props);
     this.datasetId = props.match.params.id;
+    this.dataset = data.datasets.find((ds) => ds.id == this.datasetId);
+    this.urlBuilder = new UrlBuilder();
+  }
+
+  renderModels() {
+    var metrics = new Set();
   }
 
   render() {
-    return <p>Dataset {this.datasetId} details here...</p>;
+    return (
+      <>
+        <h3>{this.dataset.dataset_name}</h3>
+        <p>{this.dataset.dataset_description}</p>
+        <table>
+          <tbody>
+            <tr>
+              <th>Source</th>
+              <td>
+                <a href={this.dataset.dataset_link} target="_blank" rel="noopener noreferer">
+                  {this.dataset.dataset_link}
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <th>Licence</th>
+              <td>MIT</td>
+            </tr>
+          </tbody>
+        </table>
+        <hr />
+        {this.renderModels()}
+      </>
+    );
   }
 }
 
