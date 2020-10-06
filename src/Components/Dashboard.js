54cd1456898520ab2bd6dc065335a3eb0a838be4
RePierre <petru.rebeja@gmail.com>
2020-10-06 15:56:16 +0300

    Added about page to the menu

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Homepage from './Homepage';
-import UrlBuilder from './UrlBuilder';
-import TaskDetails from './TaskDetails';
-import DatasetDetails from './DatasetDetails';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Homepage from "./Homepage";
+import UrlBuilder from "./UrlBuilder";
+import TaskDetails from "./TaskDetails";
+import DatasetDetails from "./DatasetDetails";
+import AboutPage from "./AboutPage";
 
 const Dashboard = (props) => {
   const { location } = props;
   return (
     <Switch location={location}>
       <Route exact path={UrlBuilder.taskUrlTemplate} component={TaskDetails} />
-      <Route exact path={UrlBuilder.datasetUrlTemplate} component={DatasetDetails} />
+      <Route
+        exact
+        path={UrlBuilder.datasetUrlTemplate}
+        component={DatasetDetails}
+      />
+      <Route exact path={UrlBuilder.aboutPageUrl} component={AboutPage} />
       <Route path="/" component={Homepage} />
     </Switch>
   );
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import { CodeIcon } from '../assets/icons';
-import '../assets/menu.scss';
+import React from "react";
+import { CodeIcon } from "../assets/icons";
+import "../assets/menu.scss";
 
 const Menu = () => (
   <div className="header-menu">
     <ul>
       <li>
-        <a href="https://github.com/eemlcommunity/ro_benchmark_leaderboard" target="_blank" rel="noopener noreferrer">
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/eemlcommunity/ro_benchmark_leaderboard"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <CodeIcon />
           <span>Code</span>
         </a>
