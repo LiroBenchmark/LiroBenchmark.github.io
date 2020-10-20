a70416e8df06c44582641a083c2e07c142a94156
RePierre <petru.rebeja@gmail.com>
2020-10-20 06:59:51 +0300

    Added link to submit page in the header menu

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,23 +1,28 @@
-import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import { LogoIcon, CodeIcon } from "../assets/icons";
-import "../assets/menu.scss";
-import UrlBuilder from "./UrlBuilder";
-import "./Header.scss";
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { LogoIcon, CodeIcon } from '../assets/icons';
+import '../assets/menu.scss';
+import UrlBuilder from './UrlBuilder';
+import './Header.scss';
 
 const Header = () => (
   <Navbar>
     <div className="logo-wrapper">
       <Navbar.Brand href="/ro_benchmark_leaderboard">
         <LogoIcon />
-        <div class="logo-text"> RoLa <br/> BENCHMARK </div>
+        <div className="logo-text">
+          {' '}
+          RoLa <br /> BENCHMARK{' '}
+        </div>
       </Navbar.Brand>
     </div>
     <Nav className="justify-content-end">
+      <Nav.Link href={UrlBuilder.submitPageUrl}>Submit</Nav.Link>
       <Nav.Link href={UrlBuilder.aboutPageUrl}>About</Nav.Link>
       <Nav.Link href={UrlBuilder.termsPageUrl}>Terms</Nav.Link>
-      <Nav.Link className="code-btn"
+      <Nav.Link
+        className="code-btn"
         href="https://github.com/eemlcommunity/ro_benchmark_leaderboard"
         target="_blank"
         rel="noopener noreferrer"
