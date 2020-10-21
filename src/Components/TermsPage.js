8ff965dc940ebbeac7819a5f7cbab9e8b3752b0e
RePierre <petru.rebeja@gmail.com>
2020-10-21 13:41:41 +0300

    Added ids to sections

--- src/Components/TermsPage.js
+++ src/Components/TermsPage.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const TermsPage = () => {
-  return <h1>Terms and Conditions</h1>;
+  return (
+    <>
+      <h1 id="terms">Terms and Conditions</h1>
+      <h1 id="privacy">Privacy notice</h1>
+    </>
+  );
 };
 
 export default TermsPage;
