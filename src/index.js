a02531e7b75da4840b1f636381c32f4c3b326b20
RePierre <petru.rebeja@gmail.com>
2020-11-18 12:34:01 +0200

    Set font family to Roboto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import WebFont from 'webfontloader';
 import App from './App';
+
+WebFont.load({
+  google: {
+    families: ['Roboto:300,400,700', 'sans-serif'],
+  },
+});
 
 ReactDOM.render(
   <BrowserRouter basename="/ro_benchmark_leaderboard">
