754218b8f0f302f487f978d7504defc5ede67fae
RePierre <petru.rebeja@gmail.com>
2020-11-18 11:20:11 +0200

    Linted index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.render(
   <BrowserRouter basename="/ro_benchmark_leaderboard">
     <Route path="/" component={App} />
   </BrowserRouter>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
