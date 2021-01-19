6f56b4df35f74f41106e0f8eba742d2b2ff25657
RePierre <petru.rebeja@gmail.com>
2021-01-19 20:52:09 +0200

    Added privacy statement page

---
+++ src/Components/PrivacyStatementPage.js
@@ -0,0 +1,181 @@
+import React from 'react';
+import './PrivacyStatementPage.scss';
+
+const PrivacyStatementPage = () => (
+  <>
+    <h1>Privacy Statement</h1>
+    <p>
+      LiRo Benchmark Project aims to process as little personal data as possible, and only in accordance with the data
+      privacy principles and regulations (in particular, the EU General Data Protection Regulation (“GDPR”)).
+    </p>
+
+    <p>
+      We are an independent structure of pro-bono volunteers with expertise in Machine Learning, NLP, linguistics and
+      data privacy and our goal is to accelerate progress in AI research on Romanian language (referred to as “we”,
+      “our”, “controller”).
+    </p>
+    <p>This Privacy Statement applies to any person involved in the LiRo Benchmark Project.</p>
+
+    <h2>What personal data do we process? For which purposes do we process such data?</h2>
+
+    <h3>If you are a user of the Benchmark, Leaderboard or Starter Code, we process the following:</h3>
+    <p>As far as we are concerned, you can submit your model without providing us any personal data.</p>
+    <ul>
+      <li>
+        However, in order to be able to allocate the tasks’ performance to your model and to maintain a record of such
+        activities, we will process personal data in the form of certain unique identifiers used by our systems only for
+        this purpose (e.g., user ID, GitHub ID).
+      </li>
+      <li>
+        Also, depending on the results of your model and on how we will develop further LiRo Benchmark Project (e.g., as
+        an award-based competition) we may ask you to provide us with certain personal data in the form of
+        identification details (e.g., name, surname) and contact details (e.g., email, phone number). We will use such
+        identification details and contact details only in order to be able to contact you for functional enquiries
+        (e.g., errors on models, additional questions on performance etc.) or to contact you for granting the award.
+      </li>
+    </ul>
+    <p>
+      We will process your personal data based on our legitimate interest to perform scientific research regarding the
+      acceleration of progress in AI on Romanian language (article 6 (1) f) GDPR), or as it is necessary for the
+      performance of our obligations to you (article 6 (1) b) GDPR). If you refuse to provide us with the personal data
+      necessary for functional enquiries or for award granting, we will not be able to provide you with such benefits.
+    </p>
+
+    <h3>If your personal data is found in the datasets, we process the following:</h3>
+    <p>
+      We will process the personal data found in the datasets only in order to develop tasks, track performance of
+      different models on such datasets and compute metrics out of such performance. We will use only personal data that
+      has been made public for information purposes, and we will further process it only for scientific research
+      purposes that are deemed compatible with the initial purposes (article 5 (1) b) GDPR).
+    </p>
+    <p>
+      Examples of such datasets are - RONEC (Romanian Named Entity Corpus) and Romanian RRT Treebank. In the near
+      future, we would like to obtain datasets from news agencies, and we will update this section as soon as possible.
+      For specific details regarding the datasets used, please access the designated section on our website.
+    </p>
+    <p>
+      We will process your personal data based on our legitimate interest to perform scientific research regarding the
+      acceleration of progress in AI on Romanian language (article 6 (1) f) GDPR).
+    </p>
+
+    <h3>
+      If you are part of the independent structure of pro-bono volunteers who are making the LiRo Benchmark Project work
+      smoothly, we thank you for this, and we process the following:
+    </h3>
+    <p>
+      We will process your name, surname and contact details in order to communicate with you about the LiRo Benchmark
+      Project (e.g., action points, implementation measures, assessments on potential issues, potential errors, schedule
+      and hold meetings, article drafting, records retention etc.).
+    </p>
+    <p>
+      We will process your personal data based on our legitimate interest to perform scientific research regarding the
+      acceleration of progress in AI on Romanian language (article 6 (1) f) GDPR).
+    </p>
+
+    <h2>Which are the parties with which we will share your personal data?</h2>
+    <p>As a rule, we will not share your personal data with third parties.</p>
+    <p>
+      However, under exceptional circumstances we may share your data with:
+      <ul>
+        <li>
+          third party services providers such as – translation offices, awards providers, cloud services providers,
+          analytics services providers, security services providers, IT services providers (GitHub);
+        </li>
+        <li>local or central public authorities, national institutions, regulators, courts of law (if requested);</li>
+        <li>any other third party to which such disclosure is expressly provided by the applicable laws.</li>
+      </ul>
+    </p>
+    <p>
+      Generally, we do not transfer your personal data to non-EU countries. However, depending on the services providers
+      used your personal data may be transferred to certain non-EU countries – more specifically, the United States of
+      America. The transfer of your personal data to a third country is performed pursuant to implementing legally
+      appropriate safeguards for the protection of your personal data (e.g., based on standard data protection clauses
+      in accordance with article 46 GDPR).
+    </p>
+
+    <h2>How long do we store your personal data?</h2>
+    <p>
+      We process and store your personal data for the period necessary to perform scientific research regarding the
+      acceleration of progress in AI on Romanian language, in accordance with the purposes mentioned under section “
+      <i>What personal data and for which purposes we process such data?</i>”
+    </p>
+
+    <h2>What are your rights related to personal data?</h2>
+    <p>
+      You have the following data protection related rights, which you may exercise either individually or cumulatively:
+      <ol>
+        <li>
+          <i>Access to your personal data</i> – you may obtain a confirmation that your personal data is processed by
+          us, information regarding such personal data, as well as a copy of such personal data,
+        </li>
+        <li>
+          <i>Correction of your personal data</i> – you may ask us to correct the incomplete or inaccurate personal data
+          that we hold about you,
+        </li>
+        <li>
+          <i>Restrict the processing of your personal data</i> - you may request us to suspend the processing of your
+          personal data during (i) the establishment of its accuracy/complete character, (ii) the determination of
+          whether our legitimate interests override your right to privacy, (iii) when the use of your data is unlawful,
+          however you oppose to its erasure, and request the restriction of their use instead or (iv) we do no longer
+          need the personal data for the purpose of the processing, but when you request the storage of such data only
+          for establishing, exercising or defending legal claims.
+        </li>
+        <li>
+          <i>Object to the processing of your personal data</i> - you may request us to no longer process your personal
+          data based on our legitimate interest when such processing affects your fundamental rights and freedoms. We
+          must cease processing, unless we cannot demonstrate any compelling legitimate interest overriding your
+          interests, rights and freedoms or unless we use such data for the establishment, exercise or defense of legal
+          claims.
+          <p>
+            <i>Important Note</i>: The access, correction, restriction and objection related rights are not applicable
+            for processing personal data for scientific research purposes, if exercising such rights would render
+            impossible or seriously impair the achievement of the specific purposes. This means that if you exercise
+            either of such rights, we will not be able to provide for an answer, if such would compromise the purpose of
+            the LiRo Benchmark Project.
+          </p>
+        </li>
+        <li>
+          <i>Erase your personal data</i> - you may request the deletion or removal of the personal data that we hold
+          about you, only in the cases expressly mentioned by the law (e.g., the data are no longer necessary for the
+          processing purposes, unlawful processing, object to processing and no overriding interest exists etc.).
+          However, we note that the exercise of such right is subject to certain exceptions that we may invoke in
+          accordance with the law (e.g., the processing is necessary for the compliance with a legal obligation, for the
+          establishment, exercise or defense of legal claims),
+          <p>
+            <i>Important Note</i>: The right to erase your personal data is not applicable to processing personal data
+            for scientific research purposes. This means that if you exercise such right, we will not be able to provide
+            for an answer.
+          </p>
+        </li>
+        <li>
+          <i>Port your personal data</i> - if we process your personal data by automated means and based on your consent
+          of for the performance of a contract, you may request a copy of your data provided in a structured, commonly
+          used, machine-readable format. You may request us to provide such data either directly to you, or to a third
+          party.
+        </li>
+        <li>
+          <i>Additional rights pertaining to automated decisions</i> - n the case of automated decisions based on your
+          personal data, you may request to (i) obtain human intervention with respect to such process, (ii) express
+          your point of view with respect to such process, (iii) contest the decision pertaining to such process.
+          <p>
+            <i>Important Note</i>: Based on the ways in which the LiRo Benchmark Project is structured at this moment,
+            the conditions that need to be fulfilled for you to exercise the right to port your data or not to be
+            subject to automated decisions are not fulfilled.
+          </p>
+        </li>
+      </ol>
+    </p>
+    <p>
+      If you wish to exercise any of the rights set out above, please contact us under the following email address
+      liro.benchmark@gmail.com. Such exercise is free of charge, unless your request is clearly unfounded, repetitive or
+      excessive, cases in which we may charge a reasonable fee or we may refuse to act on request.
+    </p>
+    <p>
+      Moreover, you have the right to file a complaint with the National Data Protection Authority (in Romania –
+      Autoritatea Nationala pentru Supravegherea Prelucrarii Datelor cu Caracter Personal) regarding any potential
+      non-compliances pertaining to the processing of your personal data within the LiRo Benchmark Project.
+    </p>
+  </>
+);
+
+export default PrivacyStatementPage;
