0f344a1324f28752a3b8c37aa3bcfd7ae32457ea
Sergiu <sergiu@Sergius-MacBook-Pro.local>
2021-04-09 20:55:49 +0300

    Solved font-family issue

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,26 +8,28 @@ class Footer extends React.Component {
     this.urlBuilder = new UrlBuilder();
   }
 
-  render = () => (
-    <footer className="footer">
-      <div className="footer-content">
-        <div className="footer-message">
-          Contact us at <a href="mailto:contact@eeml.eu">contact@eeml.eu</a>. Follow us on{' '}
-          <a href="https://twitter.com/EEMLcommunity">Twitter</a>.
+  render() {
+    return (
+      <footer className="footer">
+        <div className="footer-content">
+          <div className="footer-message">
+            Contact us at <a href="mailto:contact@eeml.eu">contact@eeml.eu</a>. Follow us on{' '}
+            <a href="https://twitter.com/EEMLcommunity">Twitter</a>.
+          </div>
+          <div className="footer-space"> </div>
+          <div className="footer-link">
+            <a href={UrlBuilder.aboutPageUrl}>About</a>
+          </div>
+          <div className="footer-link">
+            <a href={UrlBuilder.termsAndConditionsPageUrl}>Terms &amp; Conditions</a>
+          </div>
+          <div className="footer-link">
+            <a href={UrlBuilder.privacyStatementPageUrl}>Privacy statement</a>
+          </div>
         </div>
-        <div className="footer-space"> </div>
-        <div className="footer-link">
-          <a href={UrlBuilder.aboutPageUrl}>About</a>
-        </div>
-        <div className="footer-link">
-          <a href={UrlBuilder.termsAndConditionsPageUrl}>Terms &amp; Conditions</a>
-        </div>
-        <div className="footer-link">
-          <a href={UrlBuilder.privacyStatementPageUrl}>Privacy statement</a>
-        </div>
-      </div>
-    </footer>
-  );
+      </footer>
+    );
+  }
 }
 
 export default Footer;
