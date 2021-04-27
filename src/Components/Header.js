915b21bad20efd0ffc8e167e37b46e4bdae04d2c
RePierre <petru.rebeja@gmail.com>
2021-04-27 16:30:42 +0300

    Added link to datasets page in navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -22,6 +22,7 @@ const Header = () => (
     <Navbar.Collapse id="menu-responsive" className="justify-content-end">
       <Nav>
         <Nav.Link href="/ro_benchmark_leaderboard">Tasks</Nav.Link>
+        <Nav.Link href={UrlBuilder.datasetsPageUrl}>Datasets</Nav.Link>
         <Nav.Link href={UrlBuilder.submitPageUrl}>Submit</Nav.Link>
         <Nav.Link href={UrlBuilder.aboutPageUrl}>About</Nav.Link>
         <NavDropdown title="Legal" id="nav-dropdown-legal">
