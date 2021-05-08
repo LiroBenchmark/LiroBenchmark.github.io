61f46285051db8046a0a7989ed8423c01510ef33
RePierre <petru.rebeja@gmail.com>
2021-05-08 16:11:56 +0300

    Refactored ContactEmail to comply with styleguide

--- src/Components/ContactEmail.js
+++ src/Components/ContactEmail.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const ContactEmail = (props) => {
-  return <a href="mailto:liro.benchmark@gmail.com">liro.benchmark@gmail.com</a>;
-};
+const ContactEmail = () => <a href="mailto:liro.benchmark@gmail.com">liro.benchmark@gmail.com</a>;
 
 export default ContactEmail;
